0c8a6c71842ff3f60d40f2117781452917d71eb4
Personal <Personal@Lenovo>
2017-07-29 11:39:13 +0530

    first

---
+++ GitDemoProject/src/S1.java
@@ -0,0 +1,29 @@
+
+public class S1 {
+public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++)
+		{
+			int temp = 0;
+			for(int num=i; num>=1;num--)
+			{
+				
+				if((i%num)==0)
+				{
+				temp = temp+1;
+				}
+			}
+			
+			if(temp == 2)
+			{
+				System.out.println(+i+"\t");
+			}
+		}
+		System.out.println("After git");
+		System.out.println("After giti");
+		System.out.println("After git1");
+		
+		
+	}
+
+}
